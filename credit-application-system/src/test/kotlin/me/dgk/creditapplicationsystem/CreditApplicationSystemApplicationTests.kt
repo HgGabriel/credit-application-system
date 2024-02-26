f45eb1c5531cd075ab3163a5bd0ee4143403ace9
Hiago Gabriel <hiagogabrieloliveirapinto@gmail.com>
2024-02-26 19:25:04 -0300

    Iniciando o projeto

---
+++ credit-application-system/src/test/kotlin/me/dgk/creditapplicationsystem/CreditApplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.dgk.creditapplicationsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
